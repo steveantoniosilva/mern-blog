996fa2c7b942d78b4f8796974ba416ac778604ad
Steve Silva <steveantoniosilva@gmail.com>
2023-03-08 10:13:49 -0500

    merge old app & new app into 1

---
+++ client/src/BlogPost.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import './BlogPost.css';
+
+export default function BlogPost({
+  id,
+  title,
+  updateTitle,
+  content,
+  updateContent,
+  deleteBlog,
+  updateBlog,
+  isEditing,
+  setIsEditing,
+  handleEditClick,
+  newTitle,
+  newContent,
+}) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  function handleMouseEnter() {
+    setIsHovered(true);
+  }
+
+  function handleMouseLeave() {
+    setIsHovered(false);
+  }
+
+  return (
+    <div
+      className='blogPostContainer'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}>
+      <div className='blogPost-inner-container'>
+        <h2 className='title'>{title}</h2>
+        {isEditing && (
+          <input
+            className='edit-input'
+            type='text'
+            value={newTitle}
+            placeholder='new title...'
+            onChange={updateTitle}
+          />
+        )}
+        <p className='content'>{content}</p>
+        {isEditing && (
+          <textarea
+            className='edit-input'
+            type='text'
+            value={newContent}
+            placeholder='new content...'
+            onChange={updateContent}
+          />
+        )}
+      </div>
+      {isHovered && (
+        <div className='button-div'>
+          {!isEditing && (
+            <button
+              className='delete-button'
+              onClick={deleteBlog}>
+              Delete
+            </button>
+          )}
+          {isEditing && (
+            <button
+              onClick={updateBlog}
+              className='update-button'>
+              Update Blog
+            </button>
+          )}
+          <button
+            className='edit-button'
+            onClick={handleEditClick}>
+            {isEditing ? 'Cancel' : 'Edit'}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
